d15dc998df1e1e033b1d3b4b53efe99b248e32c7
Bruno <b.ramos@hotmail.com>
2020-04-07 14:49:43 -0300

    atualização exercicio

---
+++ controllers/homeController.js
@@ -0,0 +1,143 @@
+const path = require('path');
+const fs = require('fs');
+const bcrypt = require('bcrypt');
+const {check, validationResult, body} = require('express-validator')
+
+const homeController = {
+  index: (req, res) => {
+    let servicos = [
+      {nome: 'Dev full stack', imagem: '/imagens/undraw_dev_focus.svg'},
+      {nome: 'Consultoria UX', imagem: '/imagens/undraw_mobile_apps.svg'},
+      {nome: 'Marketing Digital', imagem: '/imagens/undraw_social_dashboard.svg'},
+      {nome: 'Suporte tecnico', imagem: '/imagens/undraw_dev_focus.svg'},
+      {nome: 'Data Science', imagem: '/imagens/undraw_mobile_apps.svg'},
+    ];
+
+    let banners = [
+      '/imagens/banner2.jpg',
+      '/imagens/banner3.jpg',
+      '/imagens/banner4.jpg',
+      '/imagens/banner.jpg',
+    ];
+
+    res.render('index', { title: 'Home', listaServicos: servicos, listaBanners: banners });
+  },
+  contato: (req,res) => {
+
+    let {nome, email, mensagem} =  req.body;
+    // novo conteudo
+    let infoContato = { nome, email, mensagem };
+    // caminho e nome do arquivo
+    let fileContato = path.join('db', 'contatos.json');
+    
+    let listaContato = [];
+
+    if (fs.existsSync(fileContato)){
+      // trazer informações do arquivo
+      listaContato = fs.readFileSync(fileContato, {encoding: 'utf-8'});
+      listaContato = JSON.parse(listaContato);
+    }
+    
+    //cria um array com uma posição
+    listaContato.push(infoContato);
+    //converter conteudo para json
+    listaContato = JSON.stringify(listaContato);
+    //salva informações no arquivo
+    fs.writeFileSync(fileContato, listaContato);
+
+    res.render('contato', {nome, email, title: 'Contato'});
+
+  },
+  newsletter: (req, res) => {
+    let {email} = req.query;
+
+    const fileNewsletter = path.join("db", "newsletter.json");
+
+    let listaNewsletter = [];
+
+    if (fs.existsSync(fileNewsletter)) {
+      listaNewsletter = fs.readFileSync(fileNewsletter, {encoding: "utf-8"});
+      listaNewsletter = JSON.parse(listaNewsletter);
+    }
+    
+    listaNewsletter.push({email, timestamp: new Date()});
+    listaNewsletter = JSON.stringify(listaNewsletter);
+    fs.writeFileSync(fileNewsletter, listaNewsletter);
+
+
+    res.render('newsletter', {email, title: 'Newsletter'});
+  },
+
+
+  pagCadastro : (req, res) => {
+    res.render('cadastro', {title:"Cadastro"});
+  },
+  cadastrarUsuario : (req, res) => {
+
+    let listaErrors = validationResult(req);
+
+    if(listaErrors.isEmpty()){
+
+      let {nome, email, senha} = req.body;
+
+      let senhaC = bcrypt.hashSync(senha,10);
+
+      let dadosUsuarios = {nome, email, senha:senhaC};
+      let usuarios = [];
+
+      let caminho = path.join('db', 'usuarios.json');
+      
+      if(fs.existsSync(caminho)){
+        usuarios = fs.readFileSync(caminho, {encoding : 'utf-8'} );
+        usuarios = JSON.parse(usuarios);
+      }
+
+      usuarios.push(dadosUsuarios);
+      usuarios = JSON.stringify(usuarios);
+      
+      fs.writeFileSync(caminho, usuarios);
+
+      res.render('login',{title: "Login usuario"})
+    }else{
+      return res.render('cadastro',{title:"Cadastro", errors: listaErrors.errors})
+    }     
+  },
+
+  
+  pagLogin : (req, res) => {
+    res.render('login', { title:"Login" });
+  },
+  login : (req, res) => {
+
+    let {email, senha} = req.body;
+    let caminho = path.join('db', 'usuarios.json');
+
+   
+    let usuariosSalvos = fs.readFileSync( caminho,{encoding: 'utf-8'});
+    usuariosSalvos = JSON.parse(usuariosSalvos)
+    
+    let dadosUsuario =[] ;
+
+    for( let usuario of usuariosSalvos){
+        if(usuario.email == email){
+            dadosUsuario.push(usuario)          
+        }                      
+    }
+
+    if(dadosUsuario == ""){    
+      return res.send('E-mail invalido')      
+    }
+      
+      
+    if(!bcrypt.compareSync(senha, dadosUsuario[0].senha)){     
+      return res.send('Senha invalida') 
+    }
+  
+    req.session.usuario = dadosUsuario[0];
+    
+    res.redirect('admin')
+  }
+
+};
+
+module.exports = homeController;
---
+++ routes/index.js
@@ -0,0 +1,44 @@
+const express = require('express');
+const router = express.Router();
+const {check, validationResult, body} = require('express-validator');
+const path = require('path');
+const fs = require('fs');
+const auth = require('../middlewares/auth');
+
+const homeController = require('../controllers/homeController');
+const adminController = require('../controllers/adminController');
+
+
+router.get('/', homeController.index);
+router.post('/contato', homeController.contato);
+router.get('/newsletter', homeController.newsletter);
+
+router.get('/cadastro', homeController.pagCadastro);
+router.post('/cadastro',[
+    check('nome').isLength({min:3}).withMessage("O nome deve conter no minímo 3 caracteres"),
+    check('senha').isLength({min:3}).withMessage("A senha deve conter no minímo 3 caracteres"),
+    body('email').custom( email => {
+        
+        let caminho = path.join('db' , 'usuarios.json');                
+
+        let info = fs.readFileSync( caminho, { encoding:'utf-8' });       
+        let usuario = JSON.parse(info); 
+        let existe = 0;       
+        for (const user of usuario) {
+             if(user.email === email) {
+                existe ++;
+                return;
+             }           
+        }  
+        
+        return existe == 0;      
+
+    }).withMessage("E-mail ja cadastrado!")
+], homeController.cadastrarUsuario);
+
+router.get('/login',homeController.pagLogin);
+router.post('/login',homeController.login);
+
+router.get('/admin',auth, adminController.index);
+
+module.exports = router;
